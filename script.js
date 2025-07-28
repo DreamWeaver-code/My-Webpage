c9b9d726ec74472a8908f3a22353c597bd65e662
Michael Weaver <a.weaver.michael@gmail.com>
2025-07-28 18:46:19 -0400

    added new js and also updated the about form, as well as added a contact sheet so users can submit to send to me

---
+++ script.js
@@ -0,0 +1,9 @@
+let name1= "Michael";
+//assinging a variable to my name
+let name2 = "Weaver";  
+//assinging a variable to my last name
+let fullName = name1 + " " + name2;   
+//concatenating the two variables to create my full name
+console.log(fullName); 
+
+
